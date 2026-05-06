41f332c035cafd5ade194df37430278f7e613e3c
Joeru <54849577+Joeru-san@users.noreply.github.com>
2026-05-06 19:09:16 +0200

    Add new tree prefabs and associated assets for Lowpoly Trees Package Lite
    - Created new prefab: tree_2.prefab
    - Created new prefab: tree_2_fall_001.prefab
    - Created new prefab: tree_2_snowy.prefab
    - Added ReadMe.txt with package details and usage instructions
    - Included texture asset: texture.png with metadata
    - Updated metadata files for all new assets

---
+++ Assets/Scripts/Utilities/ColliderGizmos.cs
@@ -0,0 +1,19 @@
+#if UNITY_EDITOR
+using UnityEngine;
+
+public class ColliderGizmos : MonoBehaviour
+{
+    void OnDrawGizmos()
+    {
+        BoxCollider box = GetComponent<BoxCollider>();
+        if (box == null) return;
+
+        Gizmos.color = Color.green;
+        Matrix4x4 old = Gizmos.matrix;
+        Gizmos.matrix = transform.localToWorldMatrix;
+        Gizmos.DrawWireCube(box.center, box.size);
+        Gizmos.matrix = old;
+        Gizmos.color = Color.white;
+    }
+}
+#endif
